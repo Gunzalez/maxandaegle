a17862c371ef2bdfb967ad931802c9bf993cba51
Segun Konibire <gunzalez@hotmail.com>
2018-10-13 19:52:46 +0100

    completes mobile navigation

--- website/js/maxAndAegle.js
+++ website/js/maxAndAegle.js
@@ -13,26 +13,27 @@
         }
     };
 
+
     maxAndAegle.homepageLink = {
         els: {
-
+            link: $('.js-tilt')
         },
 
         init: function () {
-
-            $('.js-tilt').tilt({
-                // maxTilt:        20,
-                // perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-                // easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-                scale:          1.1      // 2 = 200%, 1.5 = 150%, etc..
-                // speed:          300,    // Speed of the enter/exit transition.
-                // transition:     true,   // Set a transition on enter/exit.
-                // disableAxis:    null,   // What axis should be disabled. Can be X or Y.
-                // reset:          true,   // If the tilt effect has to be reset on exit.
-                // glare:          false,  // Enables glare effect
-                // maxGlare:       1       // From 0 - 1.
-            })
-
+            if(maxAndAegle.homepageLink.els.link.length){
+                maxAndAegle.homepageLink.els.link.tilt({
+                    // maxTilt:        20,
+                    // perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+                    // easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+                    scale:          1.1      // 2 = 200%, 1.5 = 150%, etc..
+                    // speed:          300,    // Speed of the enter/exit transition.
+                    // transition:     true,   // Set a transition on enter/exit.
+                    // disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+                    // reset:          true,   // If the tilt effect has to be reset on exit.
+                    // glare:          false,  // Enables glare effect
+                    // maxGlare:       1       // From 0 - 1.
+                })
+            }
         }
     };
 
@@ -52,16 +53,20 @@
                 if(maxAndAegle.navigation.status !== 'open'){
                     maxAndAegle.navigation.els.navigation.height(maxAndAegle.navigation.els.menu.height());
                     maxAndAegle.navigation.status = 'open';
+                    $(this).find('.hamburger').addClass('is-active');
                 } else {
                     maxAndAegle.navigation.els.navigation.height(0);
                     maxAndAegle.navigation.status = '';
+                    $(this).find('.hamburger').removeClass('is-active');
                 }
             })
         }
     };
 
+
     // main init
-    maxAndAegle.init = function () { // all init here
+    maxAndAegle.init = function () {
+        // all init here
         maxAndAegle.environment.init();
         maxAndAegle.navigation.init();
         maxAndAegle.homepageLink.init();
